824d37c3a8f6d63f3db668d779312a0f9bff1091
markplitt <markplitt@gmail.com>
2022-06-21 18:46:15 -0700

    fix prairie link connection (must be run locally)

--- son_of_jackfish/prarie_link_client.py
+++ son_of_jackfish/prarie_link_client.py
@@ -1,9 +1,12 @@
 import win32com.client
+import serial
 
 if __name__ == "__main__":
-    pl = win32com.client.Dispatch("PrarieLink.Application")
-    # pl = win32com.client.Dispatch("PrarieLink64.Application")
 
-    pl.Connect("128.32.173.1")
+
+    pl = win32com.client.Dispatch("PrairieLink.Application")
+    pl.Connect()
+
+
     success = pl.SendScriptCommands("-Abort")
     print(success)
