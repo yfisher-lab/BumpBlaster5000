c1a12cab9fb2ef878e5ea4d6477e66afc2232707
markplitt <markplitt@gmail.com>
2022-12-07 20:26:41 -0800

    initial completion of pg gui

--- BumpBlaster5000/vr_interface/main.py
+++ BumpBlaster5000/vr_interface/main.py
@@ -8,51 +8,51 @@ from collections import deque
 import pickle
 
 import numpy as np
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtWidgets import QApplication, QFileDialog, QInputDialog
 import pyqtgraph as pg
+from pyqtgraph import QtCore, QtGui, QtWidgets
+from pyqtgraph.QtWidgets import QFileDialog
+
 import serial
 
-from . import ui_gui
+from . import pg_gui
 from .. import params
 # import params
 from . import fictrac_utils as ft_utils
 # import utils
-from ..utils import threaded, pol2cart, numba_histogram
+from ..utils import threaded, pol2cart, numba_wrapped_histogram
 
 
-class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
+class BumpBlaster(pg_gui.WidgetWindow):
     
     
     
-    def __init__(self, parent=None):
-        super(FLUI, self).__init__(parent)
-        self.setupUi(self)
+    def __init__(self):
+        super().__init__()
 
         ## com port params
         self._params = params.FT_PC_PARAMS
 
         ## Teensy connections
-        self.start_scan_push.clicked.connect(self.start_scan)
-        self.stop_scan_push.clicked.connect(self.stop_scan)
-        self.trigger_opto_push.clicked.connect(self.trigger_opto)
+        self.start_scan_button.clicked.connect(self.start_scan)
+        self.stop_scan_button.clicked.connect(self.stop_scan)
+        self.trigger_opto_button.clicked.connect(self.trigger_opto)
         
         ## fictrac
         self.ft_frames = None
         self.ft_manager = ft_utils.FicTracSocketManager()
-        self.launch_fictrac_toggle.stateChanged.connect(self.toggle_fictrac)
+        self.launch_fictrac_checkbox.stateChanged.connect(self.toggle_fictrac)
         self._ft_process = None
         # self.save_fictrac_toggle.stateChanged.connect(self.set_fictrac_save_path)
-        self.send_orientation_toggle.stateChanged.connect(self.toggle_send_orientation)
+        self.send_orientation_checkbox.stateChanged.connect(self.toggle_send_orientation)
         self._send_orientation = threading.Event()
         self.pl_serial = None
         
 
         ## set data output directory
         # TODO: force path to be set
-        self.exp_filepath_push.clicked.connect(self.set_exp)
-        self.run_exp_push.clicked.connect(self.run_exp)
-        self.abort_exp_push.clicked.connect(self.abort_exp)
+        self.load_exp_button.clicked.connect(self.set_exp)
+        self.run_exp_button.clicked.connect(self.run_exp)
+        self.abort_exp_button.clicked.connect(self.abort_exp)
 
 
         # start serial port to send commands to teensy
@@ -68,41 +68,25 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
         while not self._isreading_teensy.is_set():
             time.sleep(.01)
 
-        # change plot widgets to remote graphics views()
-        # self.cumm_path_plotwidget = pg.widgets.RemoteGraphicsView.RemoteGraphicsView(self.centralwidget)
-        # self.cumm_path_plotwidget.setObjectName(u"cumm_path_plotwidget")
-        # self.cumm_path_plotwidget.setGeometry(QtCore.QRect(30, 220, 321, 231))
-        # self.cumm_path_plotitem = self.config_remote_plot(self.cumm_path_plotwidget)
-        self.cumm_path_plotitem = self.cumm_path_plotwidget.getPlotItem()
-
-        # self.heading_occ_plotwidget = pg.widgets.RemoteGraphicsView.RemoteGraphicsView(self.centralwidget)
-        # self.heading_occ_plotwidget.setObjectName(u"heading_occ_plotwidget")
-        # self.heading_occ_plotwidget.setGeometry(QtCore.QRect(490, 220, 321, 231))
-        # self.heading_occ_plotitem = self.config_remote_plot(self.heading_occ_plotwidget)
-        self.heading_occ_plotitem = self.heading_occ_plotwidget.getPlotItem()
-
-        # plot_buffer_time = 600 #seconds
-        # self.plot_buffers = {'integrated x': deque(maxlen=int(ft_utils.FICTRAC_FRAME_RATE*plot_buffer_time)),
-        #                     'integrated y': deque(maxlen=int(ft_utils.FICTRAC_FRAME_RATE*plot_buffer_time)),
-        #                     'heading': deque(maxlen=int(ft_utils.FICTRAC_FRAME_RATE*plot_buffer_time))}
-            
-        self.cumm_path_checkbox.stateChanged.connect(self.toggle_cumm_path)
-        self.heading_occ_checkbox.stateChanged.connect(self.toggle_heading_occ)
-            
+        #
+        self.cumm_path_plotitem = self.config_remote_plot(self.cumm_path_view)
+        self.cumm_path_plotitem.enableAutoRange(enable=True)
+        self.heading_occ_plotitem = self.config_remote_plot(self.heading_occ_view)
+        self.heading_occ_plotitem.setXRange(-.5,.5)
+        self.heading_occ_plotitem.setYRange(-.5,.5)
         
         self.plot_update_timer = QtCore.QTimer()
         self.plot_update_timer.timeout.connect(self.update_plots)
         self.plot_update_timer.start(5)
     
     @staticmethod
-    def config_remote_plot(remote_plot_widget):
-        remote_plot_widget.pg.setConfigOptions(antialias = True)
-        plot_item = remote_plot_widget.pg.PlotItem()
+    def config_remote_plot(remote_plot_view):
+        remote_plot_view.pg.setConfigOptions(antialias = True)
+        plot_item = remote_plot_view.pg.PlotItem()
         plot_item._setProxyOptions(deferGetattr=True)
         plot_item.showAxis('left', False)
         plot_item.showAxis('bottom', False)
-        plot_item.enableAutoRange(enable=True)
-        remote_plot_widget.setCentralItem(plot_item)
+        remote_plot_view.setCentralItem(plot_item)
         return plot_item
         
 
@@ -113,9 +97,9 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
         '''
 
         self.teensy_input_serial.write(b'1,0\n')  # see teensy_control.ino
-        self.start_scan_push.setEnabled(False)
-        self.trigger_opto_push.setEnabled(True)
-        self.stop_scan_push.setEnabled(True)
+        self.start_scan_button.setEnabled(False)
+        self.trigger_opto_button.setEnabled(True)
+        self.stop_scan_button.setEnabled(True)
 
         self.ft_frames = {'start': None, 'abort': None}
 
@@ -128,9 +112,9 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
         while self.ft_frames['abort'] is None:
             time.sleep(.01)
 
-        self.start_scan_push.setEnabled(True)
-        self.trigger_opto_push.setEnabled(False)
-        self.stop_scan_push.setEnabled(True)
+        self.start_scan_button.setEnabled(True)
+        self.trigger_opto_button.setEnabled(False)
+        self.stop_scan_button.setEnabled(True)
         
         # save ft_frames
         if self.ft_output_path is not None:
@@ -155,7 +139,7 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
 
     def toggle_send_orientation(self):
         
-        if self.send_orientation_toggle.isChecked():
+        if self.send_orientation_checkbox.isChecked():
             try: 
                 self.pl_serial = serial.Serial(self._params['prairie_link_com'], baudrate = self._params['baudrate'])
             except serial.SerialException:
@@ -165,16 +149,6 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
             self._send_orientation.clear()
             self.pl_serial.close()
         
-    def toggle_cumm_path(self): 
-        pass
-
-        # self.plot_buffers['integrated x'].clear()
-        # self.plot_buffers['integrated y'].clear()
-            
-    def toggle_heading_occ(self):
-        pass
-        # self.plot_buffers['heading'].clear()
-
     def set_exp(self):
         pass
 
@@ -190,7 +164,7 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
         :return:
         '''
 
-        if self.launch_fictrac_toggle.isChecked():
+        if self.launch_fictrac_checkbox.isChecked():
             # output path
             self.ft_output_path = QFileDialog.getExistingDirectory(self.centralwidget,
                                                                "FicTrac Output File")
@@ -232,10 +206,9 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
         :return:
         '''
 
-        if self.cumm_path_checkbox.isChecked():
-            self.plot_cumm_path()
-        if self.heading_occ_checkbox.isChecked():
-            self.plot_heading_occ()
+        
+        self.plot_cumm_path()
+        self.plot_heading_occ()
             
     def plot_cumm_path(self):
         # print('calling plot')
@@ -243,9 +216,9 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
                                      clear=True, _callSync='off')
         
     def plot_heading_occ(self):
-        hist, edges = numba_histogram(np.array(self.ft_manager.plot_deques['heading']), 20)
+        hist, edges = numba_wrapped_histogram(np.array(self.ft_manager.plot_deques['heading']), 20)
         x, y = pol2cart(hist, edges[1:])
-        self.heading_occ_plotitem.plot(x,y, fillLevel=.5,clear=True, _callSync='off')
+        self.heading_occ_plotitem.plot(x,y, fillLevel=1,clear=True, _callSync='off')
         
 
 
@@ -257,9 +230,7 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
         '''
 
         # close fictrac
-        self.run_ft_evnt.clear()
-        self._ft_process.join()
-        self._read_ft_handle.join()
+        
         if self.pl_serial is not None:
             self.pl_serial.close()
 
@@ -278,27 +249,8 @@ class FLUI(QtWidgets.QMainWindow, ui_gui.Ui_MainWindow):
         self.disconnect()
         event.accept()
 
-
-class FicTracPlots:
     
-    def __init__(ft_running_event, plot_deques):
-        
-        app = pg.mkQApp()
-        
-        win = pg.LayoutWidget()
-        win.resize(300,300)
-        pg.setConfigOptions(antialias=True)
-        
-        
-
-
-def main():
-    app = QApplication(sys.argv)
-    form = FLUI()
-    form.show()
-    r = app.exec_()
-    sys.exit(r)
-
 
 if __name__ == '__main__':
-    main()
+    bb = BumpBlaster()
+    pg.exec()
