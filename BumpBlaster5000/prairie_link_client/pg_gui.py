1d8d45ed7a7fd81d43b56ba4a049f4be49d8aabe
markplitt <markplitt@gmail.com>
2022-12-08 16:19:07 -0800

    unwrap histogram, begin pl window code

---
+++ BumpBlaster5000/prairie_link_client/pg_gui.py
@@ -0,0 +1,40 @@
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtWidgets, QtGui
+
+class MainWindow(object):
+    def __init__(self):
+        self.app = pg.mkQApp("BumpBlaster5000 Interface")
+        
+        self.layout = pg.LayoutWidget()
+        
+        # number of slices
+        self.num_slices_label = QtWidgets.QLabel()
+        self.num_slices_label.setText("Number of Slices:")
+        self.num_slices_input = QtWidgets.QLineEdit()
+        
+        # begin read raw data stream
+        self.start_rrds_button = QtWidgets.QPushButton("Begin ReadRawDataStream")
+        
+        # ch 1 viewing
+        
+        
+        
+        
+        
+        self.layout.addWidget(self.num_slices_label)
+        self.layout.addWidget(self.num_slices_input)
+        
+        self.layout.addWidget(self.start_rrds_button)
+        
+        self.layout.show()
+        
+        
+class RemotePlottingWindow(object):
+    
+    def __init__(self):
+        self.app = pg.mkQApp("Remote Plotting Window")
+        
+if __name__ == '__main__':
+    ww = MainWindow()
+    pg.exec()
