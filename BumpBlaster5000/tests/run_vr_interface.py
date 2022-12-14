ceb8d1862a1f90a9a788ebf1e1a286aa918ef74c
markplitt <markplitt@gmail.com>
2022-12-14 13:42:50 -0800

    file cleanup

--- BumpBlaster5000/tests/run_vr_interface.py
+++ BumpBlaster5000/tests/run_vr_interface.py
@@ -2,16 +2,8 @@ import pyqtgraph as pg
 import BumpBlaster5000 as bb
 import BumpBlaster5000.vr_interface
 
-import sys
+
 
 if __name__ == "__main__":
     ui = bb.vr_interface.main.BumpBlaster()
     pg.exec()
-    # app = QApplication(sys.argv)
-    # form = bb.vr_interface.main.FLUI()
-    # form.show()
-    # r = app.exec_()
-    # sys.exit(r)
-
-
-    # bb.vr_interface.main.main()
