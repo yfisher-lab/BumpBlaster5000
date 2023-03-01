2b47b0b9c02f4fbe22b83be0b7455f9d6ab5a1a6
Yvette <evettita@gmail.com>
2023-02-28 19:51:40 -0800

    add thick bar

--- BumpBlaster5000/experiment_protocols/G3_files/create_bar_pattern_8pix.m
+++ BumpBlaster5000/experiment_protocols/G3_files/create_bar_pattern_8pix.m
@@ -18,6 +18,7 @@ pattern.Pats(:,1:8,11*npix:end,1)=1;
 
 
 pattern.Pats = pattern.Pats(:,:,end:-1:1,:);
+pattern.Pats(:,:,:,2) = 0;
 size(pattern.Pats)
 pattern.Panel_map = [22 21 20 19 18 17 16 15 14 13 12;
                      11 10 9 8 7 6 5 4 3 2 1];
--- BumpBlaster5000/experiment_protocols/__init__.py
+++ BumpBlaster5000/experiment_protocols/__init__.py
@@ -1,1 +1,2 @@
-from . import fixed_0deg_remap, fixed_180deg_remap, open_loop_cw, random_remap
+from . import fixed_0deg_remap, fixed_180deg_remap
+from . import open_loop_cw, random_remap, flash_bar
