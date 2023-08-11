960a59af531054a8075fb9a2ee508722c3105740
Yvette <evettita@gmail.com>
2023-08-11 15:33:38 -0700

    update experiment protocols

--- BumpBlaster5000/experiment_protocols/dangling_fly_two_sec_pairing.py
+++ BumpBlaster5000/experiment_protocols/dangling_fly_two_sec_pairing.py
@@ -18,40 +18,47 @@ def build_cmd_str(queue):
     unpaired_heading = int(max_dac_val/3*2 - 4096/24)
     
     
-    cmd_len = 5*(2*5*4 + 4 + 1)
-    cmd = [cmd_len, 10]
-    
-    # flash each bar for 2 seconds 5 times interleaved with 5 seconds between
+    # # flash each bar for 2 seconds 5 times interleaved with 5 seconds between
     for _ in range(5):
+        cmd = [int(5*6), 10]
         # heading, index, opto_bool, opto_delay, combined_dur
-        cmd.extend([3800, 0, 0, 0, 5000])
+        cmd.extend([paired_heading, 4095, 0, 0, 4900])
         cmd.extend([paired_heading, 0, 0, 0, 2000])
-        cmd.extend([3800, 0, 0, 0, 5000])
+        cmd.extend([paired_heading, 4095, 0, 0, 100])
+        cmd.extend([unpaired_heading, 4095, 0, 0, 4900])
         cmd.extend([unpaired_heading, 0, 0, 0, 2000])
-      
+        cmd.extend([unpaired_heading, 4095, 0, 0, 100])
+        queue.put(cmd_to_str(cmd).encode('UTF-8'))
+        sleep(14.1)
+
+   
+    for _ in range(3):
+        cmd = [int(5*6), 10]
+        # pair 1 bar with opto stim, while the other one just flashes
+        cmd.extend([paired_heading, 4095, 0, 0, 4900])
+        cmd.extend([paired_heading, 0, 1, 0, 2000])
+        cmd.extend([paired_heading, 4095, 0, 0, 100])
+        cmd.extend([unpaired_heading, 4095, 0, 0, 4900])
+        cmd.extend([unpaired_heading, 0, 0, 0, 2000])
+        cmd.extend([unpaired_heading, 4095, 0, 0, 100])
+        queue.put(cmd_to_str(cmd).encode('UTF-8'))
+        sleep(14.1)
     
-    # pair 1 bar with opto stim, while the other one just flashes
-    cmd.extend([3800, 0, 0, 0, 5000])
-    cmd.extend([paired_heading, 0, 1, 0, 2000])
-    cmd.extend([3800, 0, 0, 0, 5000])
-    cmd.extend([unpaired_heading, 0, 0, 0, 2000])
-    
-    # flash each bar again for 2 seconds 5 times interleaved
+
     for _ in range(5):
-        cmd.extend([3800, 0, 0, 0, 5000])
+        cmd = [int(5*6), 10]
+        # heading, index, opto_bool, opto_delay, combined_dur
+        cmd.extend([paired_heading, 4095, 0, 0, 4900])
         cmd.extend([paired_heading, 0, 0, 0, 2000])
-        cmd.extend([3800, 0, 0, 0, 5000])
+        cmd.extend([paired_heading, 4095, 0, 0, 100])
+        cmd.extend([unpaired_heading, 4095, 0, 0, 4900])
         cmd.extend([unpaired_heading, 0, 0, 0, 2000])
+        cmd.extend([unpaired_heading, 4095, 0, 0, 100])
+        queue.put(cmd_to_str(cmd).encode('UTF-8'))
+        sleep(14.1)
 
-    cmd.extend([0, 4095, 0, 0, 5000])
-    cmd_str = ""
-    for item in cmd:
-        cmd_str = cmd_str + f"{item},"
-    cmd_str = cmd_str[:-1] + "\n"
 
-    queue.put(cmd_str.encode('UTF-8'))
-
-    sleep(14*(5*2 + 1) + 6)
+  
     queue.put('0,5\n'.encode('UTF-8'))
     queue.put('1,7,4095\n'.encode('UTF-8'))
 
@@ -60,6 +67,12 @@ def build_cmd_str(queue):
     
     return 
     
+def cmd_to_str(cmd):
+    cmd_str = ""
+    for item in cmd:
+        cmd_str = cmd_str + f"{item},"
+    cmd_str = cmd_str[:-1] + "\n"
+    return cmd_str
     
 def run(queue):
   
--- BumpBlaster5000/experiment_protocols/open_loop_cw_ccw.py
+++ BumpBlaster5000/experiment_protocols/open_loop_cw_ccw.py
@@ -30,16 +30,24 @@ def build_cmd_str(queue):
         queue.put(cmd.encode('UTF-8'))
     # cmd.extend([0,  4095,   0,  0, 5000])
     queue.put('1,7,4095\n'.encode('UTF-8'))
+    sleep(2)
+
+     
+    for r in range(n_reps):
+        for h in headings[::-1].tolist():
+            cmd = '2, 8, ' + f"{h}" + ', 0\n' 
+            queue.put(cmd.encode('UTF-8'))
+            sleep(.2)
+            # cmd.extend([h, 0, 1, 0, 1875])
+        cmd = f'2,8,{h},4095\n'
+        queue.put(cmd.encode('UTF-8'))
+        cmd = f'2,8,0,4095\n'
+        queue.put(cmd.encode('UTF-8'))
+    # cmd.extend([0,  4095,   0,  0, 5000])
+    queue.put('1,7,4095\n'.encode('UTF-8'))
+
     sleep(5)
-
-    # cmd_str = ""
-    # for item in cmd:
-    #     cmd_str = cmd_str + f"{item},"
-    # cmd_str = cmd_str[:-1] + "\n"
-
-    # queue.put(cmd_str.encode('UTF-8'))
-
-    # sleep(26.1)
+   
     queue.put('0,5\n'.encode('UTF-8'))
     queue.put('1,7,0\n'.encode('UTF-8'))
 
