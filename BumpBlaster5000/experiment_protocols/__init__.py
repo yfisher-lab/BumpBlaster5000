ec05df8e9dab48b09990286549bb89fb7c0b4e35
Yvette <evettita@gmail.com>
2023-10-13 09:47:59 -0700

    Changed bar jump file name to remove dash

--- BumpBlaster5000/experiment_protocols/__init__.py
+++ BumpBlaster5000/experiment_protocols/__init__.py
@@ -1,3 +1,4 @@
 from . import fixed_0deg_remap, fixed_180deg_remap
 from . import open_loop_cw, open_loop_cw_ccw, random_remap, flash_bar, dark_trial_3min
 from . import dangling_fly_two_sec_pairing, dangling_fly_two_sec_pairing_reversed
+from . import bar_jump_v2, bar_jump_v2_longer
