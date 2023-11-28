99612e9dcc34731f35f30b436bb2baebcaaeff84
Yvette <evettita@gmail.com>
2023-11-28 14:52:41 -0800

    bit bang

--- BumpBlaster5000/experiment_protocols/__init__.py
+++ BumpBlaster5000/experiment_protocols/__init__.py
@@ -1,5 +1,5 @@
 from . import fixed_0deg_remap, fixed_180deg_remap
 from . import open_loop_cw, open_loop_cw_ccw, random_remap, flash_bar, dark_trial_3min
 from . import dangling_fly_two_sec_pairing, dangling_fly_two_sec_pairing_reversed
-from . import bar_jump_v2, bar_jump_v2_longer, bar_jump_v2_20s_trials, bar_jump_v2_30s_trials
-from . import bar_jump_v2_60s_trials, bar_jump_v2_test
+# from . import bar_jump_v2, bar_jump_v2_longer, bar_jump_v2_20s_trials, bar_jump_v2_30s_trials
+# from . import bar_jump_v2_60s_trials, bar_jump_v2_test
