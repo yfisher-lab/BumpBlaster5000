67540c9fd1d8265786d065a70a2a82f1174cdf68
Yvette <evettita@gmail.com>
2025-02-14 15:07:15 -0800

    testing open loop squares

--- BumpBlaster5000/experiment_protocols/test_closed_loop_squares.py
+++ BumpBlaster5000/experiment_protocols/test_closed_loop_squares.py
@@ -9,28 +9,33 @@ def build_cmd_str(queue):
     n_spots = 96 # one per pixel
     n_reps = 2
     n_indexes = 5 # 5 scenes, 4 square elevations and 1 dark
+    n_rotations = 2 # cw and ccw
 
     headings = np.arange(0, 2*np.pi, 2*np.pi/n_spots)
     indexes = np.arange(0, max_dac_val+1, max_dac_val/n_indexes)
 
-    cmd_len = 5*(n_spots+1)
+    n_pr_inputs = 5 # num inputs to PointRunner (heading, index, opto_bool, opto_delay, combined_dur)
+    cmd_len = n_pr_inputs*n_reps*n_rotations*(n_spots + 1)
     cmd = [cmd_len, 10]
 
     dark = indexes[-1]
 
     frame_dur = 100 # ms
+    dark_dur = 4000 # ms
 
     # cw/ccw spin followed by dark
-    i = indexes[2]
-    # for h in headings.tolist(): # ccw
-    #     cmd.extend([h, i, 0, 0, frame_dur])
+    i = indexes[2] 
 
-    # cmd.extend([0, dark, 0, 0, 4000]) # dark period
+    for r in range(n_reps):
+        for h in headings[::-1].tolist(): # cw
+            cmd.extend([h, i, 0, 0, frame_dur])
+        
+        cmd.extend([0, dark, 0, 0, dark_dur]) # dark period between elevations
 
-    for h in headings[::-1].tolist(): # cw
-        cmd.extend([h, i, 0, 0, frame_dur])
-    
-    cmd.extend([0, dark, 0, 0, 4000]) # dark period between elevations
+        for h in headings.tolist(): # ccw
+            cmd.extend([h, i, 0, 0, frame_dur])
+
+        cmd.extend([0, dark, 0, 0, dark_dur]) # dark period
 
     cmd_str = ""
     for item in cmd:
@@ -38,7 +43,8 @@ def build_cmd_str(queue):
     cmd_str = cmd_str[:-1] + "\n"
 
     queue.put(cmd_str.encode('UTF-8'))
-    sleep((((n_spots*frame_dur) + 4000))/1000) # sec, this needs to be the approximate length of the open loop duration or else it moves on to the next queue immediately
+    cl_dur = n_reps*n_rotations*(((n_spots*frame_dur)+dark_dur)/1000)
+    sleep(2*n_reps*(((n_spots*frame_dur) + 4000))/1000) # sec, this needs to be the approximate length of the open loop duration or else it moves on to the next queue immediately
 
     queue.put('0,5\n'.encode('UTF-8')) # go back into closed loop
     queue.put('1,7,0\n'.encode('UTF-8')) # set index back to 0
