2cb5bd798cd1bd70487afa0b33dc158b230afc90
Yvette <evettita@gmail.com>
2025-02-14 13:30:02 -0800

    modify test_closed_loop_squares while testing on arena

--- BumpBlaster5000/experiment_protocols/test_closed_loop_squares.py
+++ BumpBlaster5000/experiment_protocols/test_closed_loop_squares.py
@@ -11,20 +11,26 @@ def build_cmd_str(queue):
     n_indexes = 5 # 5 scenes, 4 square elevations and 1 dark
 
     headings = np.arange(0, 2*np.pi, 2*np.pi/n_spots)
-    indexes = np.arange(0, 5000, 5000/n_indexes)
+    indexes = np.arange(0, max_dac_val+1, max_dac_val/n_indexes)
 
     cmd_len = 5*(n_spots+1)
     cmd = [cmd_len, 10]
 
     dark = indexes[-1]
 
-    frame_dur = 50 # ms
+    frame_dur = 100 # ms
 
     # cw/ccw spin followed by dark
-    for h in headings.tolist():
-        cmd.extend([h, 0, 0, 0, frame_dur])
+    i = indexes[2]
+    # for h in headings.tolist(): # ccw
+    #     cmd.extend([h, i, 0, 0, frame_dur])
 
-    cmd.extend([0, dark, 0, 0, 4000]) # dark period
+    # cmd.extend([0, dark, 0, 0, 4000]) # dark period
+
+    for h in headings[::-1].tolist(): # cw
+        cmd.extend([h, i, 0, 0, frame_dur])
+    
+    cmd.extend([0, dark, 0, 0, 4000]) # dark period between elevations
 
     cmd_str = ""
     for item in cmd:
@@ -32,9 +38,10 @@ def build_cmd_str(queue):
     cmd_str = cmd_str[:-1] + "\n"
 
     queue.put(cmd_str.encode('UTF-8'))
-    sleep(10)
+    sleep((((n_spots*frame_dur) + 4000))/1000) # sec, this needs to be the approximate length of the open loop duration or else it moves on to the next queue immediately
 
     queue.put('0,5\n'.encode('UTF-8')) # go back into closed loop
+    queue.put('1,7,0\n'.encode('UTF-8')) # set index back to 0
 
 def run(queue):
 
